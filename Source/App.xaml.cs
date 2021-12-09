215102fb852716597a409497f246ed4fc9ec2c80
dounai2333 <1328600509@qq.com>
2021-12-09 14:49:01 +0800

    note

--- Source/App.xaml.cs
+++ Source/App.xaml.cs
@@ -14,89 +14,6 @@ using Microsoft.Win32;
 
 namespace CSO2_ComboLauncher
 {
-    public static class MiniDumpHelper
-    {
-        // Taken almost verbatim from http://blog.kalmbach-software.de/2008/12/13/writing-minidumps-in-c/
-        [Flags]
-        public enum Option : uint
-        {
-            // From dbghelp.h:
-            Normal = 0x00000000,
-            WithDataSegs = 0x00000001,
-            WithFullMemory = 0x00000002,
-            WithHandleData = 0x00000004,
-            FilterMemory = 0x00000008,
-            ScanMemory = 0x00000010,
-            WithUnloadedModules = 0x00000020,
-            WithIndirectlyReferencedMemory = 0x00000040,
-            FilterModulePaths = 0x00000080,
-            WithProcessThreadData = 0x00000100,
-            WithPrivateReadWriteMemory = 0x00000200,
-            WithoutOptionalData = 0x00000400,
-            WithFullMemoryInfo = 0x00000800,
-            WithThreadInfo = 0x00001000,
-            WithCodeSegs = 0x00002000,
-            WithoutAuxiliaryState = 0x00004000,
-            WithFullAuxiliaryState = 0x00008000,
-            WithPrivateWriteCopyMemory = 0x00010000,
-            IgnoreInaccessibleMemory = 0x00020000,
-            ValidTypeFlags = 0x0003ffff
-        };
-
-        public enum ExceptionInfo
-        {
-            None,
-            Present
-        }
-
-        [StructLayout(LayoutKind.Sequential, Pack = 4)]  // Pack=4 is important! So it works also for x64!
-
-        public struct MiniDumpExceptionInformation
-        {
-            public uint ThreadId;
-
-            public IntPtr ExceptionPointers;
-
-            [MarshalAs(UnmanagedType.Bool)]
-            public bool ClientPointers;
-        }
-
-        // Overload requiring MiniDumpExceptionInformation
-        [DllImport("dbghelp.dll", EntryPoint = "MiniDumpWriteDump", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = true)]
-        static extern bool MiniDumpWriteDump(IntPtr hProcess, uint processId, SafeHandle hFile, uint dumpType, ref MiniDumpExceptionInformation expParam, IntPtr userStreamParam, IntPtr callbackParam);
-
-        // Overload supporting MiniDumpExceptionInformation == NULL
-        [DllImport("dbghelp.dll", EntryPoint = "MiniDumpWriteDump", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = true)]
-        static extern bool MiniDumpWriteDump(IntPtr hProcess, uint processId, SafeHandle hFile, uint dumpType, IntPtr expParam, IntPtr userStreamParam, IntPtr callbackParam);
-
-        [DllImport("kernel32.dll", EntryPoint = "GetCurrentThreadId", ExactSpelling = true)]
-        static extern uint GetCurrentThreadId();
-
-        public static bool Write(SafeHandle fileHandle, Option options, ExceptionInfo exceptionInfo)
-        {
-            Process currentProcess = Process.GetCurrentProcess();
-            IntPtr currentProcessHandle = currentProcess.Handle;
-            uint currentProcessId = (uint)currentProcess.Id;
-            MiniDumpExceptionInformation exp;
-            exp.ThreadId = GetCurrentThreadId();
-            exp.ClientPointers = false;
-            exp.ExceptionPointers = IntPtr.Zero;
-            if (exceptionInfo == ExceptionInfo.Present)
-                exp.ExceptionPointers = Marshal.GetExceptionPointers();
-
-            if (exp.ExceptionPointers == IntPtr.Zero)
-                return MiniDumpWriteDump(currentProcessHandle, currentProcessId, fileHandle, (uint)options, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
-            else
-                return MiniDumpWriteDump(currentProcessHandle, currentProcessId, fileHandle, (uint)options, ref exp, IntPtr.Zero, IntPtr.Zero);
-        }
-
-        public static bool Write(string dmpPath, Option dumpType)
-        {
-            using (FileStream stream = new FileStream(dmpPath, FileMode.Create))
-                return Write(stream.SafeFileHandle, dumpType, ExceptionInfo.Present);
-        }
-    }
-
     public partial class App : Application
     {
         public App()
@@ -192,6 +109,8 @@ namespace CSO2_ComboLauncher
             Download.StartLoop();
             new ZipWorker();
             ZipWorker.StartLoop();
+
+            // force to use Tls 1.2 so some PC that has default setting with Tls 1.1 will works
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
         }
 
@@ -281,6 +200,89 @@ namespace CSO2_ComboLauncher
                             , MessageBoxButton.OK
                             , MessageBoxImage.Error);
             Environment.Exit(ex.HResult);
+        }
+    }
+
+    public static class MiniDumpHelper
+    {
+        // Taken almost verbatim from http://blog.kalmbach-software.de/2008/12/13/writing-minidumps-in-c/
+        [Flags]
+        public enum Option : uint
+        {
+            // From dbghelp.h:
+            Normal = 0x00000000,
+            WithDataSegs = 0x00000001,
+            WithFullMemory = 0x00000002,
+            WithHandleData = 0x00000004,
+            FilterMemory = 0x00000008,
+            ScanMemory = 0x00000010,
+            WithUnloadedModules = 0x00000020,
+            WithIndirectlyReferencedMemory = 0x00000040,
+            FilterModulePaths = 0x00000080,
+            WithProcessThreadData = 0x00000100,
+            WithPrivateReadWriteMemory = 0x00000200,
+            WithoutOptionalData = 0x00000400,
+            WithFullMemoryInfo = 0x00000800,
+            WithThreadInfo = 0x00001000,
+            WithCodeSegs = 0x00002000,
+            WithoutAuxiliaryState = 0x00004000,
+            WithFullAuxiliaryState = 0x00008000,
+            WithPrivateWriteCopyMemory = 0x00010000,
+            IgnoreInaccessibleMemory = 0x00020000,
+            ValidTypeFlags = 0x0003ffff
+        };
+
+        public enum ExceptionInfo
+        {
+            None,
+            Present
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 4)]  // Pack=4 is important! So it works also for x64!
+
+        public struct MiniDumpExceptionInformation
+        {
+            public uint ThreadId;
+
+            public IntPtr ExceptionPointers;
+
+            [MarshalAs(UnmanagedType.Bool)]
+            public bool ClientPointers;
+        }
+
+        // Overload requiring MiniDumpExceptionInformation
+        [DllImport("dbghelp.dll", EntryPoint = "MiniDumpWriteDump", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = true)]
+        static extern bool MiniDumpWriteDump(IntPtr hProcess, uint processId, SafeHandle hFile, uint dumpType, ref MiniDumpExceptionInformation expParam, IntPtr userStreamParam, IntPtr callbackParam);
+
+        // Overload supporting MiniDumpExceptionInformation == NULL
+        [DllImport("dbghelp.dll", EntryPoint = "MiniDumpWriteDump", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = true)]
+        static extern bool MiniDumpWriteDump(IntPtr hProcess, uint processId, SafeHandle hFile, uint dumpType, IntPtr expParam, IntPtr userStreamParam, IntPtr callbackParam);
+
+        [DllImport("kernel32.dll", EntryPoint = "GetCurrentThreadId", ExactSpelling = true)]
+        static extern uint GetCurrentThreadId();
+
+        public static bool Write(SafeHandle fileHandle, Option options, ExceptionInfo exceptionInfo)
+        {
+            Process currentProcess = Process.GetCurrentProcess();
+            IntPtr currentProcessHandle = currentProcess.Handle;
+            uint currentProcessId = (uint)currentProcess.Id;
+            MiniDumpExceptionInformation exp;
+            exp.ThreadId = GetCurrentThreadId();
+            exp.ClientPointers = false;
+            exp.ExceptionPointers = IntPtr.Zero;
+            if (exceptionInfo == ExceptionInfo.Present)
+                exp.ExceptionPointers = Marshal.GetExceptionPointers();
+
+            if (exp.ExceptionPointers == IntPtr.Zero)
+                return MiniDumpWriteDump(currentProcessHandle, currentProcessId, fileHandle, (uint)options, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+            else
+                return MiniDumpWriteDump(currentProcessHandle, currentProcessId, fileHandle, (uint)options, ref exp, IntPtr.Zero, IntPtr.Zero);
+        }
+
+        public static bool Write(string dmpPath, Option dumpType)
+        {
+            using (FileStream stream = new FileStream(dmpPath, FileMode.Create))
+                return Write(stream.SafeFileHandle, dumpType, ExceptionInfo.Present);
         }
     }
 }
